8177ff18149ee329c64967ee6a3a811761a13963
Chirag <chiragsonar1718@gmail.com>
2023-05-07 07:57:32 +0530

    Created new model for cart, post route for cart

--- model/Cart.js
+++ model/Cart.js
@@ -5,28 +5,14 @@ const cartSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
-  items: [
-    {
-      productId: {
-        type: String,
-        required: true,
-      },
-      name: {
-        type: String,
-        required: true,
-      },
-      quantity: {
-        type: Number,
-        required: true,
-        min: [1, "Quantity can not be less than 1."],
-        default: 1,
-      },
-      price: {
-        type: Number,
-        required: true,
-      },
-    },
-  ],
+
+  productId: {
+    type: String,
+    required: true,
+  },
+  quantity: {
+    type: Number,
+  },
 });
 
 module.exports = mongoose.model("Cart", cartSchema);
--- routes/cart.js
+++ routes/cart.js
@@ -2,7 +2,6 @@ const router = require("express").Router();
 
 // Cart Schema
 const Cart = require("../model/Cart");
-const Product = require("../model/Product");
 
 // ROUTES 👇🏻
 
@@ -10,26 +9,41 @@ const Product = require("../model/Product");
 router.get("/get-all-items", async (req, res) => {
   try {
     const results = await Cart.find({});
+    if (!results) {
+      return res.status(404).send("Cart not found!");
+    }
+    console.log("this is get-all-items cart route");
     res.status(200).send(results);
   } catch (err) {
     console.log(err, "Error: Cart get-all-items route");
   }
 });
 
-// Get one Cart Item
-router.get("/get-one-item/:id", async (req, res) => {
+// Add Item to cart
+router.post("/add-item", async (req, res) => {
   try {
+    // Check if current user already has this product in his cart
+    // If true then update quantity or else add new item
+    const currentProduct = await Cart.findOne({
+      userId: req.body.userId,
+      productId: req.body.productId,
+    });
+    if (currentProduct) {
+      currentProduct.quantity = currentProduct.quantity + 1;
+      currentProduct.save();
+      return res.status(200).send(currentProduct);
+    } else {
+      const cart = new Cart({
+        userId: req.body.userId,
+        productId: req.body.productId,
+        quantity: 1,
+      });
+      const savedCart = await cart.save();
+      return res.status(200).send(savedCart);
+    }
   } catch (err) {
-    console.log(err, "Error Cart get-one-item route");
+    console.log(err);
   }
 });
 
-// Add Item to cart
-router.post("/add-item", async (req, res) => {
-  const userId = req.params.id;
-  const productId = req.body.productId;
-  const quantity = req.body.quantity;
-  const item = Product.findOne({ _id: productId });
-});
-
-module.exports = router
+module.exports = router;
